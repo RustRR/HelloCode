bee475ef530067760757bb8106e09551c80147b8
RustR <reimovrr@mail.ru>
2023-03-12 20:59:00 +0600

    Example 005 ConditionElse

---
+++ Example005_ConditionElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLowerInvariant() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
